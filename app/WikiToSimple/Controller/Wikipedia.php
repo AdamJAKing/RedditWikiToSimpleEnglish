059fee7c87dbfbbf6ea490a51cc0abebb64b064d
AdamJAKing <Adam@whiteoctober.co.uk>
2016-03-11 10:49:42 +0000

    Changed Wikipedia to final/utility based class. Added 2 new helper functions

--- app/WikiToSimple/Controller/Wikipedia.php
+++ app/WikiToSimple/Controller/Wikipedia.php
@@ -2,15 +2,37 @@
 
 namespace WikiToSimple\Controller;
 
-class Wikipedia
+/**
+ * Class Wikipedia
+ * @package WikiToSimple\Controller
+ *
+ * A simple helper/utility class that provides integration for Wikipedia based tasks
+ */
+
+final class Wikipedia
 {
 
-    function searchForArticle($language, $article)
+    static function searchForArticle($language, $article)
     {
         $url = "https://" . $language . ".wikipedia.org/w/api.php?action=query&titles=" . $article . "&format=json";
+        $urlContents = file_get_contents($url);
+
+        $urlJson = json_decode($urlContents, true);
+
+        if (isset($urlJson['query']['pages']['-1'])) {
+            echo "Page doesn't exist\n";
+
+            return false;
+        }
+
+        return true;
     }
 
-    function createSafeAPIArticleFromLink($originalLink) {
+    /**
+     * @param $originalLink A Link to a Wikipedia article
+     * @return Returns a safe API article name on success or null on failure
+     */
+    static function createSafeAPIArticleFromLink($originalLink) {
         if (stripos($originalLink, "/wiki/") !== false) {
 
             $articleNameStart = stripos($originalLink, "/wiki/") + 6;
@@ -29,6 +51,26 @@ class Wikipedia
 
             return $safeArticleName;
         }
+        return;
+    }
+
+    /**
+     * @param $url URL containing original language
+     * @param $oldLang Old language to replace
+     * @param $newLang New language to append
+     * @return New url on success or null on failure
+     */
+
+    static function changeArticleUrLLanguage($url, $oldLang, $newLang) {
+
+        $langStart = stripos($url, $oldLang);
+
+        if ($langStart !== false) {
+            $newUrl = substr_replace($url, $newLang, $langStart, strlen($oldLang));
+
+            return $newUrl;
+        }
+
         return;
     }
 }
